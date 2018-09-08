88a5555ffa2cb3b6010ceabf91646e95044554e9
n.nikolas <n.nikolas@vegaitsourcing.rs>
2018-09-08 11:49:58 +0200

    Added Umbraco Models and Controllers for managment and teams, players, seasons, results...

---
+++ LZRNS.Web/Controllers/RenderMvc/LeagueManagmentController.cs
@@ -0,0 +1,18 @@
+﻿using LZRNS.Models.DocumentTypes.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace LZRNS.Web.Controllers.RenderMvc
+{
+    public class LeagueManagmentController : RenderMvcController
+    {
+        public ActionResult Index(LeagueManagmentModel model)
+        {
+            return CurrentTemplate(model);
+        }
+    }
+}
---
+++ LZRNS.Web/Controllers/RenderMvc/TeamsController.cs
@@ -0,0 +1,18 @@
+﻿using LZRNS.Models.DocumentTypes.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace LZRNS.Web.Controllers.RenderMvc
+{
+    public class TeamsController : RenderMvcController
+    {
+        public ActionResult Index(TeamsModel model)
+        {
+            return CurrentTemplate(model);
+        }
+    }
+}
