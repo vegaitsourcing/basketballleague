ecb3459f0f94bc4ee76688f9dcdd7bd64593e0ac
Jovica Ostojic <j.ostojic@vegaitsourcing.rs>
2018-09-08 14:42:15 +0200

    Results

--- html/js/default.js
+++ html/js/default.js
@@ -20,6 +20,7 @@ let app = {
 		// call your functions here
 		functions.initSlider();
 		functions.mobileNav();
+		functions.popup();
 		// functions.objectFit();
 		if(('ontouchstart' in window || navigator.msMaxTouchPoints > 0) && window.matchMedia('screen and (max-width: 1024px)').matches) {
 			$('html').addClass('touch');
