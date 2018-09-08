691ad6a04c916a84cf697b47ef24e46f448636f7
r.bacic <rajko@emakina.rs>
2018-09-08 19:44:51 +0200

    data upload

---
+++ LZRNS.Web/Controllers/RenderMvc/ImportController.cs
@@ -0,0 +1,51 @@
+﻿using LZRNS.Models.DocumentTypes.Pages;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace LZRNS.Web.Controllers.RenderMvc
+{
+    public class ImportController : RenderMvcController
+    {
+        public ActionResult Index(ImportModel model)
+        {
+            return CurrentTemplate(model);
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase[] files)
+        {
+            var content = new ImportModel();
+            if (files == null || files.Length == 0)
+            {
+                var currentPageId = UmbracoContext.PageId;
+                
+
+                return CurrentTemplate(content);
+            }
+
+            foreach(var file in files)
+            {
+                if(file != null)
+                {
+                    var memStr = GetFileAsMemoryStream(file);
+                }
+            }
+
+            return CurrentTemplate(content);
+        }
+
+        private MemoryStream GetFileAsMemoryStream(HttpPostedFileBase uploadedFile)
+        {
+            var buf = new byte[uploadedFile.InputStream.Length];
+            uploadedFile.InputStream.Read(buf, 0, (int)uploadedFile.InputStream.Length);
+            MemoryStream memStr = new MemoryStream(buf);
+            return memStr;
+        }
+    }
+}
