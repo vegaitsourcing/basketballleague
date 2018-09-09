395de4df4839c46c593cca36af7b5312a5267671
StrahinjaGrbic <strajagrbic@gmail.com>
2018-09-09 18:36:19 +0200

    Added TeamHistoryItem

---
+++ LZRNS.DomainModels/Helper/TeamHistoryItem.cs
@@ -0,0 +1,45 @@
+﻿using LZRNS.DomainModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LZRNS.DomainModels.Helper
+{
+    public class TeamHistoryItem
+    {
+        public Season Season { get; set; }
+
+        public int Place
+        {
+            get
+            {
+                int place = 0;
+
+                ICollection<Team> orderedTeams = League.LeagueSeasons.FirstOrDefault(ls => ls.SeasonId == Season.Id).
+                    Teams.OrderByDescending(t => t.Points).ToList();
+
+                foreach (Team team in orderedTeams)
+                {
+                    place++;
+                    if (team.Id == Team.Id)
+                    {
+                        break;
+                    }
+                }
+                return place;
+            }
+        }
+
+        public Team Team { get; set; }
+
+        public League League
+        {
+            get
+            {
+                return Team.LeagueSeason.League;
+            }
+        }
+    }
+}
