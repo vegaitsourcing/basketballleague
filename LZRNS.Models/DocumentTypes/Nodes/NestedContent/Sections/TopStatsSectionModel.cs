80240641b503e812e6a3af733b3c545386d5ddd0
Maksim Bogunović <m.bogunovic@vegaitsorucing.rs>
2018-10-24 12:58:11 +0200

    TopStatsSectionModel

--- LZRNS.Models/DocumentTypes/Nodes/NestedContent/Sections/TopStatsSectionModel.cs
+++ LZRNS.Models/DocumentTypes/Nodes/NestedContent/Sections/TopStatsSectionModel.cs
@@ -1,4 +1,6 @@
-﻿using Umbraco.Core.Models;
+﻿using LZRNS.Models.Extensions;
+using Umbraco.Core.Models;
+using Umbraco.Web;
 
 namespace LZRNS.Models.DocumentTypes.Nodes.NestedContent.Sections
 {
@@ -6,6 +8,15 @@ namespace LZRNS.Models.DocumentTypes.Nodes.NestedContent.Sections
 	{
 		public TopStatsSectionModel(IPublishedContent content) : base(content)
 		{
+			UmbracoHelper = new UmbracoHelper(UmbracoContext.Current);
 		}
+
+		protected UmbracoHelper UmbracoHelper { get; }
+
+		public string RoundName => this.GetCachedValue(() =>
+			UmbracoHelper.GetSingleContentOfType<StatisticsSettingsModel>()?.ResultsRound);
+
+		public int? SeasonYearStart => this.GetCachedValue(() =>
+			UmbracoHelper.GetSingleContentOfType<StatisticsSettingsModel>()?.SeasonYearStart);
 	}
 }
