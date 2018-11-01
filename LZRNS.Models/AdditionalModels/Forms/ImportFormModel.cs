7849770b176507fb1b5561527ddae49bfd1d5ff9
Darko Opala <d.opala@vegaitsourcing.rs>
2018-11-01 09:42:43 +0100

    Updated Import form and controller

---
+++ LZRNS.Models/AdditionalModels/Forms/ImportFormModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace LZRNS.Models.AdditionalModels.Forms
+{
+	public class ImportFormModel
+	{
+		[Required]
+		public HttpPostedFileBase[] Files { get; set; }
+	}
+}
