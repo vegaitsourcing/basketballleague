b7bc9d0da36d916452ab0e4b44caef3763a65210
Sasa Stevcic <s.stevcic@vegaitsourcing.rs>
2019-08-26 09:58:01 +0200

    Updated css

--- LZRNS.Web/js/pages/statistics.js
+++ LZRNS.Web/js/pages/statistics.js
@@ -30,21 +30,6 @@ let statistics = {
 
 			window.location.search = seasonParam;
 		});
-		
-		$('.statistic-page').on('click', '[data-table-popup-link]', function () {
-			const $this = $(this);
-			const popupName = $this.data('table-popup-link');
-			const $popup = $('.popup-overlay[data-popup-window="' + popupName + '"]').addClass('popup-overlay--active');
-			const tableType = $this.data('table-type');
-			const $tableDetails = $this.find('[data-result-type=' + tableType + ']');
-
-			if ($tableDetails.length === 0) {
-				return;
-			}
-
-			$popup.find('.popup-placeholder').html($tableDetails.html());
-			$popup.fadeIn(400);
-		});
 	}
 };
 
