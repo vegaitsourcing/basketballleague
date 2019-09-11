3293dd788cba327d22e53675dba669deec252d97
Danilo Novaković <dakenzi97@gmail.com>
2019-09-11 13:50:11 +0200

    KLRNS-0022 | Danilo Novakovic | Refactored and fixed bug where LeaderboardLandingController would cause exception if there was no data in database

--- LZRNS.Web/Controllers/RenderMvc/LeaderboardLandingController.cs
+++ LZRNS.Web/Controllers/RenderMvc/LeaderboardLandingController.cs
@@ -1,43 +1,68 @@
-﻿using System.Linq;
+﻿using LZRNS.DomainModels.Models;
+using LZRNS.DomainModels.Repository.Interfaces;
 using LZRNS.Models.DocumentTypes.Pages;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using Umbraco.Web.Mvc;
-using LZRNS.DomainModels.Repository.Interfaces;
-using System;
 
 namespace LZRNS.Web.Controllers.RenderMvc
 {
-	public class LeaderboardLandingController : RenderMvcController
-	{
-		private readonly ISeasonRepository _seasonRepo;
+    public class LeaderboardLandingController : RenderMvcController
+    {
+        private readonly ISeasonRepository _seasonRepo;
 
-		public LeaderboardLandingController(ISeasonRepository seasonRepo)
-		{
-			_seasonRepo = seasonRepo;
-		}
+        public LeaderboardLandingController(ISeasonRepository seasonRepo)
+        {
+            _seasonRepo = seasonRepo;
+        }
 
-		public ActionResult Index(LeaderboardLandingModel model, string ln, string r)
-		{
-			model.CurrentShownLeague = !string.IsNullOrWhiteSpace(ln) ? ln : model.Leagues.FirstOrDefault();
-            //NOTE:lexicographical comparison, bug
-            //model.AllRounds = _seasonRepo.GetSeasonByYear(model.SeasonYearStart).LeagueSeasons
-            //    .First(x => x.League.Name.Equals(model.CurrentShownLeague)).Rounds
-            //    //.Where(y => y.RoundName.CompareTo(model.RoundName) <= 0)
-            //    .Select(z => z.RoundName)
-            //    .OrderBy(k => k).ToList();
+        public ActionResult Index(LeaderboardLandingModel model, string ln, string r)
+        {
+            model.CurrentShownLeague = GetCurrentShownLeague(model.Leagues, ln);
 
-            
-            var allRounds = _seasonRepo.GetSeasonByYear(model.SeasonYearStart).LeagueSeasons
-                .First(x => x.League.Name.Equals(model.CurrentShownLeague)).Rounds
-               // .Where(y => y.RoundName.CompareTo(model.RoundName) <= 0)
-                .Select(z => z.RoundName);
-                
-           int maxLen = allRounds.Max(x => x.Length);
+            var roundNames = GetAllRoundsByLeagueNameAndYear(model.CurrentShownLeague, model.SeasonYearStart);
+            model.AllRounds = SortRoundNames(roundNames);
 
-            model.AllRounds = allRounds.OrderBy(k => k.PadLeft(maxLen, '0')).ToList();
-			
-			model.CurrentShownRound = !string.IsNullOrWhiteSpace(r) ? r : model.AllRounds.First();
+            model.CurrentShownRound = GetCurrentShownRound(model.AllRounds, r);
+
             return CurrentTemplate(model);
-		}
-	}
+        }
+
+        private static string GetCurrentShownLeague(IEnumerable<string> leagues, string leagueName)
+        {
+            return !string.IsNullOrWhiteSpace(leagueName) ? leagueName : leagues.FirstOrDefault();
+        }
+
+        private static string GetCurrentShownRound(IEnumerable<string> rounds, string round)
+        {
+            return !string.IsNullOrWhiteSpace(round) ? round : rounds.FirstOrDefault();
+        }
+
+        private static IEnumerable<string> GetRoundNames(LeagueSeason league)
+        {
+            return league?.Rounds?.Select(z => z.RoundName) ?? new List<string>();
+        }
+
+        private static List<string> SortRoundNames(IEnumerable<string> roundNames)
+        {
+            var names = roundNames.ToList();
+            if (names.Count == 0) return new List<string>();
+            int maxRoundNameLength = names.Max(x => x.Length);
+            return names.OrderBy(k => k.PadLeft(maxRoundNameLength, '0')).ToList();
+        }
+
+        private IEnumerable<string> GetAllRoundsByLeagueNameAndYear(string leagueName, int year)
+        {
+            var league = GetLeagueByNameAndYear(leagueName, year);
+            return GetRoundNames(league);
+        }
+
+        private LeagueSeason GetLeagueByNameAndYear(string leagueName, int year)
+        {
+            var startYearSeason = _seasonRepo.GetSeasonByYear(year);
+            var leagueSeasons = startYearSeason?.LeagueSeasons ?? new List<LeagueSeason>();
+            return leagueSeasons.FirstOrDefault(x => x.League.Name.Equals(leagueName));
+        }
+    }
 }
