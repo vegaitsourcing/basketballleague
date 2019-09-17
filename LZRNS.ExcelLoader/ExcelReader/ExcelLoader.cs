31a6d1b99958c193c76f51cefdabe55ff68e9963
Danilo Novaković <dakenzi97@gmail.com>
2019-09-17 11:44:56 +0200

    Refactored ExcelLoader

--- LZRNS.ExcelLoader/ExcelReader/ExcelLoader.cs
+++ LZRNS.ExcelLoader/ExcelReader/ExcelLoader.cs
@@ -1,5 +1,4 @@
 ﻿using ClosedXML.Excel;
-using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -8,100 +7,66 @@ namespace LZRNS.ExcelLoader.ExcelReader
 {
     public class ExcelLoader : AbstractExcelLoader
     {
-        #region Private Fields
-        // key represent team name
-        private Dictionary<string, TeamStatistic> teams;
+        public Dictionary<string, TeamStatistic> Teams { get; }
 
-        private Dictionary<string, Dictionary<string, List<PlayerScore>>> playerScores;
-        #endregion Private Fields
+        public Dictionary<string, Dictionary<string, List<PlayerScore>>> PlayerScores { get; }
 
-        #region Properies 
-
-        public Dictionary<string, TeamStatistic> Teams
-        {
-            get { return teams; }
-        }
-
-        public Dictionary<string, Dictionary<string, List<PlayerScore>>> PlayerScores
-        {
-            get { return playerScores; }
-        }
-        #endregion Properies
-
-
-        #region Constructors
         public ExcelLoader(string configPath) : base(configPath)
         {
-            Loger.log.Debug("Start main proces");
-            teams = new Dictionary<string, TeamStatistic>();
-            playerScores = new Dictionary<string, Dictionary<string, List<PlayerScore>>>();
-
+            Loger.log.Debug("Start main process");
+            Teams = new Dictionary<string, TeamStatistic>();
+            PlayerScores = new Dictionary<string, Dictionary<string, List<PlayerScore>>>();
         }
-        #endregion Constructors
 
-        #region Public Methods
         public Dictionary<string, List<PlayerScore>> GetTeamScores(string teamName)
         {
-            Dictionary<string, List<PlayerScore>> teamScores;
-            PlayerScores.TryGetValue(teamName, out teamScores);
+            PlayerScores.TryGetValue(teamName, out var teamScores);
             return teamScores;
-
         }
+
         public List<PlayerScore> GetPlayerScoreList(string teamName, string playerFullName)
         {
+            var teamScores = GetTeamScores(teamName);
 
-            Dictionary<string, List<PlayerScore>> teamScores = GetTeamScores(teamName);
+            List<PlayerScore> playerScores = null;
 
-            List<PlayerScore> scores = null;
-            if (teamScores != null)
-            {
-                teamScores.TryGetValue(playerFullName, out scores);
-            }
+            teamScores?.TryGetValue(playerFullName, out playerScores);
 
-            return scores;
+            return playerScores;
         }
 
-        #endregion Public Methods
-
-        #region Private Methods
-        private void ProcessSheet(IXLWorksheet sheet, ref TeamStatistic teamStatistic, out bool isEmptyPage)
+        private void ProcessSheet(IXLWorksheet sheet, ref TeamStatistic teamStatistic)
         {
             Loger.log.Debug("ProcessSheet started for table: " + sheet.Name);
-            Stopwatch stopwatch = new Stopwatch();
+            var stopwatch = new Stopwatch();
             stopwatch.Start();
-            isEmptyPage = false;
 
-            IXLRows rows = sheet.RowsUsed();
+            var rows = sheet.RowsUsed();
 
-            TeamScore teamScore = new TeamScore();
-            teamScore.RoundName = sheet.Name;
-            int currentRowNo;
+            var teamScore = new TeamScore
+            {
+                RoundName = sheet.Name
+            };
 
-            IEnumerable<FieldItem> globalFields = Mapper.Fields.FindAll(i => i.GlobalField == true);
-            currentRowNo = Mapper.Fields.First().RowIndex;
-
-            // we are incrasing rowNo because currentRow reprenset row where is header placed!
-            if (CheckIfPageIsEmpty(rows, currentRowNo + 1, Mapper.Fields.First().ColumnIndex))
+            IEnumerable<FieldItem> globalFields = Mapper.Fields.FindAll(i => i?.GlobalField == true);
+            int currentRowNo = Mapper.Fields[0].RowIndex;
+            int headerRowNo = currentRowNo + 1;
+            if (CheckIfPageIsEmpty(rows, headerRowNo, Mapper.Fields[0].ColumnIndex))
             {
                 Loger.log.Debug("ProcessSheet: Sheet: " + sheet.Name + ", is empty for Team: " + teamStatistic.TeamName);
-                isEmptyPage = true;
                 return;
             }
 
-            // we are incrasing rowNo because currentRow reprenset row where is header placed!
-            PopulateModelField(teamScore, rows, globalFields, currentRowNo + 1);
+            PopulateModelField(teamScore, rows, globalFields, headerRowNo);
 
-            // When we start to load data for each player, we must take row number of headers and then increase it for 1
-            currentRowNo = Mapper.Fields.First().RowIndex;
-            IEnumerable<FieldItem> otherFields = Mapper.Fields.FindAll(i => i.GlobalField == false);
-            int playerCount = CalculatePlayerCount(rows, currentRowNo, otherFields.First().ColumnIndex, MaxPlayerPerMatch);
+            var otherFields = Mapper.Fields.FindAll(i => i?.GlobalField == false);
+            int playerCount = CalculatePlayerCount(rows, currentRowNo, otherFields[0].ColumnIndex, MaxPlayerPerMatch);
 
             for (int i = 1; i < playerCount; i++)
             {
                 currentRowNo++;
-                PlayerScore pl = new PlayerScore();
+                var pl = new PlayerScore();
                 PopulateModelField(pl, rows, otherFields, currentRowNo);
-                /*contains list of all players score*/
                 pl.AgainstTeam = teamScore.AgainstTeam;
                 teamScore.AddPlayerScore(pl);
 
@@ -109,54 +74,33 @@ namespace LZRNS.ExcelLoader.ExcelReader
                 AddPlayerInTeam(teamStatistic.TeamName, pl.NameAndLastName);
             }
 
-            Loger.log.Debug("ProcessSheet: ENDED for sheet: " + sheet.Name + ", timeElapsed: " + stopwatch.Elapsed);
-
-            stopwatch.Stop();
             teamStatistic.AddTeamScore(teamScore);
 
+            stopwatch.Stop();
+            Loger.log.Debug("ProcessSheet: ENDED for sheet: " + sheet.Name + ", timeElapsed: " + stopwatch.Elapsed);
         }
 
-        //not good solution, hack
-        private void SetPlayerScoreTeam(TeamScore teamScore)
-        {
-            foreach (PlayerScore playerScore in teamScore.PlayerScores)
-            {
-                playerScore.AgainstTeam = teamScore.AgainstTeam;
-            }
-        }
         private void AddPlayerScore(string teamName, PlayerScore ps)
         {
-            List<PlayerScore> list;
-            Dictionary<string, List<PlayerScore>> teamScore;
-            if (!teamName.Equals(String.Empty))
+            if (teamName.Equals(string.Empty)) return;
+
+            if (!PlayerScores.TryGetValue(teamName, out var teamScore))
             {
-                if (playerScores.TryGetValue(teamName, out teamScore))
-                {
-                    if (!ps.NameAndLastName.Equals(String.Empty))
-                    {
-                        if (teamScore.TryGetValue(ps.NameAndLastName, out list))
-                        {
-                            list.Add(ps);
-                        }
+                var playerScoreList = new List<PlayerScore> { ps };
+                var scoresDictionary = new Dictionary<string, List<PlayerScore>> { { ps.NameAndLastName, playerScoreList } };
+                PlayerScores.Add(teamName, scoresDictionary);
+                return;
+            }
 
-                        else
-                        {
-                            playerScores[teamName].Add(ps.NameAndLastName, new List<PlayerScore>() { ps });
-                        }
+            if (ps.NameAndLastName.Equals(string.Empty)) return;
 
-                    }
-
-
-                }
-                else
-                {
-                    var playerScoreList = new List<PlayerScore>() { ps };
-                    var scoresDictionary = new Dictionary<string, List<PlayerScore>>() { { ps.NameAndLastName, playerScoreList } };
-                    PlayerScores.Add(teamName, scoresDictionary);
-                }
-
-
-
+            if (teamScore.TryGetValue(ps.NameAndLastName, out var list))
+            {
+                list.Add(ps);
+            }
+            else
+            {
+                PlayerScores[teamName].Add(ps.NameAndLastName, new List<PlayerScore> { ps });
             }
         }
 
@@ -165,28 +109,18 @@ namespace LZRNS.ExcelLoader.ExcelReader
             string teamName = CheckFileStructureAndExtractTeamName(exApp, fileName);
             int currentSheetNo = 0;
 
-            TeamStatistic teamStatistic = new TeamStatistic(teamName);
-            bool isPageEmpty;
+            var teamStatistic = new TeamStatistic(teamName);
 
-            foreach (IXLWorksheet sheet in Sheets)
+            foreach (var sheet in Sheets)
             {
-                // for odd sheet we want to skip loading
                 if (currentSheetNo % 2 == 0)
                 {
-                    ///process sheet by sheet
-                    ProcessSheet(sheet, ref teamStatistic, out isPageEmpty);
-
+                    ProcessSheet(sheet, ref teamStatistic);
                 }
                 currentSheetNo++;
             }
 
-            teams[teamStatistic.TeamName] = teamStatistic;
+            Teams[teamStatistic.TeamName] = teamStatistic;
         }
-
-        #endregion Private Methods
-
-
-
-
     }
 }
