54ad0f6eeb222b6e5304736e216316c07e0a1be1
Danilo Novaković <dakenzi97@gmail.com>
2019-09-17 12:54:57 +0200

    Refactored PlayerScore, TeamScore and TeamStatistic

--- LZRNS.ExcelLoader/Model/TeamStatistic.cs
+++ LZRNS.ExcelLoader/Model/TeamStatistic.cs
@@ -1,38 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace LZRNS.ExcelLoader
 {
     public class TeamStatistic
     {
-        private String teamName = "-";
-        private List<TeamScore> teamScores;
-
-        public TeamStatistic(String tName)
+        public TeamStatistic(string tName)
         {
-            teamName = tName;
-            teamScores = new List<TeamScore>();
+            TeamName = tName ?? "-";
+            TeamScores = new List<TeamScore>();
         }
 
-        #region Properies
-        public String TeamName
+        public string TeamName { get; }
+
+        public List<TeamScore> TeamScores { get; }
+
+        public void AddTeamScore(TeamScore ts)
         {
-            get { return teamName; }
+            TeamScores.Add(ts);
         }
-
-        public List<TeamScore> TeamScores
-        {
-            get { return teamScores; }
-        }
-        #endregion Properies
-
-        #region Public Methods
-        public void AddTeamScore (TeamScore ts)
-        {
-            teamScores.Add(ts);
-        }
-        #endregion Public Methods
-
-
     }
 }
