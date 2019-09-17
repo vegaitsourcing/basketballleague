6b310471a71c46ee4a3b54780ad102815282034d
Danilo Novaković <dakenzi97@gmail.com>
2019-09-17 11:33:36 +0200

    Refactored ExcelAnalyzer

--- LZRNS.ExcelLoader/ExcelReader/ExcelAnalyzer.cs
+++ LZRNS.ExcelLoader/ExcelReader/ExcelAnalyzer.cs
@@ -1,140 +1,89 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using ClosedXML.Excel;
+﻿using ClosedXML.Excel;
 using LZRNS.ExcelLoader.Model;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 namespace LZRNS.ExcelLoader.ExcelReader
 {
     public class ExcelAnalyzer : AbstractExcelLoader
     {
+        public Dictionary<string, Dictionary<string, List<PlayerInfo>>> TeamPlayerInfos { get; set; }
 
-        #region Private Fields
-
-        private const int _maxPlayerPerMatch = 12;
-
-        private XLWorkbook _exApp;
-        private IXLWorksheets _sheets;
-        private Dictionary<string, Dictionary<string, List<PlayerInfo>>> _teamPlayerInfos;
-
-        #endregion
-
-        #region Properties
-        public Dictionary<string, Dictionary<string, List<PlayerInfo>>> TeamPlayerInfos
-        {
-            get { return _teamPlayerInfos; }
-            set { _teamPlayerInfos = value; }
-        }
-        #endregion Properties
-
-
-
-        #region Constructors
         public ExcelAnalyzer(string configPath) : base(configPath)
         {
-            Loger.log.Debug("Start main proces");
-            _teamPlayerInfos = new Dictionary<string, Dictionary<string, List<PlayerInfo>>>();
-
+            Loger.log.Debug("Start main process");
+            TeamPlayerInfos = new Dictionary<string, Dictionary<string, List<PlayerInfo>>>();
         }
-        #endregion Constructors
-
-        #region Public Methods
-
 
         public override void Load(XLWorkbook exApp, string fileName)
         {
-            //fileName should have the following format - teamName-seasonName-leagueName.xlsx (at least one file)
             string teamName = CheckFileStructureAndExtractTeamName(exApp, fileName);
             int currentSheetNo = 0;
-            bool isPageEmpty;
 
-            foreach (IXLWorksheet sheet in Sheets)
+            foreach (var sheet in Sheets)
             {
-                // for odd sheet we want to skip loading
                 if (currentSheetNo % 2 == 0)
                 {
-                    ///process sheet by sheet
-                    ProcessSheet(sheet, teamName, out isPageEmpty);
-
+                    ProcessSheet(sheet, teamName);
                 }
                 currentSheetNo++;
             }
-
         }
 
-        #endregion
-
-        #region Private Methods
-        private void ProcessSheet(IXLWorksheet sheet, string teamName, out bool isEmptyPage)
+        private void ProcessSheet(IXLWorksheet sheet, string teamName)
         {
             Loger.log.Debug("ProcessSheet started for table: " + sheet.Name);
-            Stopwatch stopwatch = new Stopwatch();
+
+            var stopwatch = new Stopwatch();
             stopwatch.Start();
-            isEmptyPage = false;
 
-            IXLRows rows = sheet.RowsUsed();
-            int currentRowNo;
+            var rows = sheet.RowsUsed();
 
-            IEnumerable<FieldItem> globalFields = Mapper.Fields.FindAll(i => i.GlobalField == true);
-            currentRowNo = Mapper.Fields.First().RowIndex;
+            int currentRowNo = Mapper.Fields[0].RowIndex;
+            int headerRowNo = currentRowNo + 1;
 
-            // we are incrasing rowNo because currentRow reprenset row where is header placed!
-            if (CheckIfPageIsEmpty(rows, currentRowNo + 1, Mapper.Fields.First().ColumnIndex))
+            if (CheckIfPageIsEmpty(rows, headerRowNo, Mapper.Fields[0].ColumnIndex))
             {
-                isEmptyPage = true;
                 return;
             }
 
-            // When we start to load data for each player, we must take row number of headers and then increase it for 1
-            currentRowNo = Mapper.Fields.First().RowIndex;
-            IEnumerable<FieldItem> otherFields = Mapper.Fields.FindAll(i => i.GlobalField == false);
+            IEnumerable<FieldItem> otherFields = Mapper.Fields.FindAll(i => i?.GlobalField == false);
 
-            int playerCount = CalculatePlayerCount(rows, currentRowNo, otherFields.First().ColumnIndex, _maxPlayerPerMatch);
+            int playerCount = CalculatePlayerCount(rows, currentRowNo, otherFields.First().ColumnIndex, MaxPlayerPerMatch);
 
             for (int i = 1; i < playerCount; i++)
             {
                 currentRowNo++;
-                PlayerInfo pi = new PlayerInfo();
+                var pi = new PlayerInfo();
                 PopulateModelField(pi, rows, otherFields, currentRowNo);
-                /*contains list of all players score*/
                 AddPlayerInfo(teamName, pi);
             }
-            Loger.log.Debug("ProcessSheet: ENDED for sheet: " + sheet.Name + ", timeElapsed: " + stopwatch.Elapsed);
+
             stopwatch.Stop();
 
+            Loger.log.Debug("ProcessSheet: ENDED for sheet: " + sheet.Name + ", timeElapsed: " + stopwatch.Elapsed);
         }
 
         private void AddPlayerInfo(string teamName, PlayerInfo pi)
         {
-            List<PlayerInfo> list;
-            Dictionary<string, List<PlayerInfo>> infoDict;
+            if (pi.NameAndLastName.Equals(string.Empty))
+                return;
 
-            if (!pi.NameAndLastName.Equals(String.Empty))
+            if (!TeamPlayerInfos.TryGetValue(teamName, out var infoDict))
             {
-                if (TeamPlayerInfos.TryGetValue(teamName, out infoDict))
-                {
-                    if (infoDict.TryGetValue(pi.NameAndLastName, out list))
-                    {
-                        list.Add(pi);
-                    }
-                    else
-                    {
-                        TeamPlayerInfos[teamName].Add(pi.NameAndLastName, new List<PlayerInfo>() { pi });
-                    }
-
-                }
-                else
-                {
-                    TeamPlayerInfos.Add(teamName, new Dictionary<string, List<PlayerInfo>>() { { pi.NameAndLastName, new List<PlayerInfo>() } });
-                }
+                TeamPlayerInfos.Add(teamName, new Dictionary<string, List<PlayerInfo>> { { pi.NameAndLastName, new List<PlayerInfo>() } });
+                return;
             }
 
-
+            if (infoDict.TryGetValue(pi.NameAndLastName, out var list))
+            {
+                list.Add(pi);
+            }
+            else
+            {
+                TeamPlayerInfos[teamName].Add(pi.NameAndLastName, new List<PlayerInfo> { pi });
+            }
         }
-
-
-        #endregion
-
     }
 }
