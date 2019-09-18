ed34725d3705a7a85e35760ff5677a800008d4f9
Danilo Novaković <dakenzi97@gmail.com>
2019-09-18 14:37:11 +0200

    Added getall_db_data.sql utility script that can be used for debugging data in database.

--- delete_db_data.sql
+++ delete_db_data.sql
@@ -8,12 +8,3 @@ DELETE Round;
 DELETE PlayerPerTeam;
 DELETE Player;
 DELETE Team;
---necessary instances for site build
-/*
-0328e63c-f50c-413b-b19e-11510ec52d25
-924407bc-9cbf-406b-9dd1-f5de44807cb5
-f16c3522-a856-4513-9f59-844ed3293b87
-*/
-INSERT INTO Season(Id, Name, SeasonStartYear) VALUES ('0328e63c-f50c-413b-b19e-11510ec52d25', '2016', 2016); 
-INSERT INTO League(Id, Name) VALUES ('924407bc-9cbf-406b-9dd1-f5de44807cb5', 'Liga A');
-INSERT INTO LeagueSeason(Id, SeasonId, LeagueId) VALUES('f16c3522-a856-4513-9f59-844ed3293b87', '0328e63c-f50c-413b-b19e-11510ec52d25', '924407bc-9cbf-406b-9dd1-f5de44807cb5');
--- klrns_id_generate.js
+++ klrns_id_generate.js
@@ -2,25 +2,25 @@
  * This is an utility javascript function that can help you generate
  * custom id following the `KLRNS-<number>: <description>` convention
  * which is used for this project on `trello.com`.
- * 
+ *
  * To use it go to trello, open console window and just copy paste
  * code in this file.
  */
 
 /**
- * Returns titles of all trello tasks that start with `KLRNS` sorted.
+ * @returns {Array<string>} Sorted titles of all trello tasks that start with `KLRNS`.
  */
-function getKlrnsArray(){
-	let titles = document.querySelectorAll('span.list-card-title.js-card-name')
-	let klrnsArray= [];
-	titles.forEach(title => {
-		const titleText = title.innerText;
-		if(/^\s*?klrns/i.test(titleText)){
-			klrnsArray.push(titleText);
-		}
-	});
-	klrnsArray.sort();
-	return klrnsArray;
+function getKlrnsArray() {
+    let titles = document.querySelectorAll("span.list-card-title.js-card-name");
+    let klrnsArray = [];
+    titles.forEach(title => {
+        const titleText = title.innerText;
+        if (/^\s*?klrns/i.test(titleText)) {
+            klrnsArray.push(titleText);
+        }
+    });
+    klrnsArray.sort();
+    return klrnsArray;
 }
 
-console.table(getKlrnsArray())
+console.table(getKlrnsArray());
