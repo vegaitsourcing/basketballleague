13a1836300f3ae0229d6d4432e16ec7a3885e1f1
Danilo Novaković <dakenzi97@gmail.com>
2019-09-19 15:29:30 +0200

    Finished writing tests for RoundScheduler

--- LZRNS.Core.Tests/RoundSchedulerTests.cs
+++ LZRNS.Core.Tests/RoundSchedulerTests.cs
@@ -25,10 +25,56 @@ namespace LZRNS.Core.Tests
             var options = new RoundScheduleOptions(firstRoundStartDate: DateTime.Today.AddDays(2));
 
             var scheduledRounds = _sut.ScheduleRounds(rounds, options);
+
             var firstRound = scheduledRounds.First();
             var firstGame = firstRound.Games.First();
 
             Assert.True(AreSameDay(firstGame.DateTime, options.FirstRoundStartDate), $"Expected '{firstGame.DateTime}' to be same day as '{options.FirstRoundStartDate}'");
+        }
+
+        [Fact]
+        public void ScheduleRounds_SetsStartTimeOfFirstGameInRoundAccordingToOptions()
+        {
+            var rounds = GetRoundsWithGames(2);
+            var options = new RoundScheduleOptions(roundStartTime: TimeSpan.FromHours(5));
+
+            var scheduledRounds = _sut.ScheduleRounds(rounds, options);
+
+            var firstRound = scheduledRounds.First();
+            var firstGame = firstRound.Games.First();
+
+            Assert.Equal(options.RoundStartTime.Hours, firstGame.DateTime.Hour);
+        }
+
+        [Fact]
+        public void ScheduleRounds_SetIntervalBetweenGamesAccordingToOptions()
+        {
+            var rounds = GetRoundsWithGames(numberOfRounds: 1, numberOfGames: 2);
+            var options = new RoundScheduleOptions(intervalBetweenGamesInMinutes: 10);
+
+            var scheduledRounds = _sut.ScheduleRounds(rounds, options);
+
+            var firstRound = scheduledRounds.First();
+            var firstGame = firstRound.Games.First();
+            var secondGame = firstRound.Games.Skip(1).First();
+            var timeSpan = secondGame.DateTime - firstGame.DateTime;
+
+            Assert.Equal(options.IntervalBetweenGamesInMinutes, (uint)timeSpan.Minutes);
+        }
+
+        [Fact]
+        public void ScheduleRounds_SetIntervalBetweenRoundsAccordingToOptions()
+        {
+            var rounds = GetRoundsWithGames(numberOfRounds: 2);
+            var options = new RoundScheduleOptions(intervalBetweenRoundsInDays: 2);
+
+            var scheduledRounds = _sut.ScheduleRounds(rounds, options).ToList();
+
+            var firstRoundDate = scheduledRounds[0].Games.First().DateTime;
+            var secondRoundDate = scheduledRounds[1].Games.First().DateTime;
+            var timeSpan = secondRoundDate - firstRoundDate;
+
+            Assert.Equal(options.IntervalBetweenRoundsInDays, (uint)timeSpan.Days);
         }
 
         private static bool AreSameDay(DateTime firstDate, DateTime secondDate)
