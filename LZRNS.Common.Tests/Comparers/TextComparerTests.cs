9dcb6b082c05e27485132c83ba12fcf882305669
Danilo Novaković <dakenzi97@gmail.com>
2019-09-25 09:30:38 +0200

    KLRNS-0036 | Danilo Novakovic | Implemented TextComparer using LevenshteinDistance algorithm

---
+++ LZRNS.Common.Tests/Comparers/TextComparerTests.cs
@@ -0,0 +1,53 @@
+﻿using LZRNS.Common.Comparers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LZRNS.Common.Comparers.Tests
+{
+    public class TextComparerTests
+    {
+        private readonly TextComparer _sut;
+        private readonly List<string> _stringList = new List<string> { "apsolventi", "becej", "blejkersi", "celarevo" };
+
+        public TextComparerTests()
+        {
+            _sut = new TextComparer();
+        }
+
+        [Theory]
+        [InlineData("apsolventi", "apsolventi")]
+        public void GetMostSimilar_InputExistsInCollection_ReturnInput(string input, string expected)
+        {
+            string actual = _sut.GetMostSimilar(_stringList, input);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("apsol ven ti", "apsolventi")]
+        public void GetMostSimilar_InputIsDifferentBySpaces_ReturnMostSimilar(string input, string expected)
+        {
+            string actual = _sut.GetMostSimilar(_stringList, input);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("apsloventi", "apsolventi")]
+        public void GetMostSimilar_InputIsMisspeled_ReturnMostSimilar(string input, string expected)
+        {
+            string actual = _sut.GetMostSimilar(_stringList, input);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("bečej", "becej")]
+        public void GetMostSimilar_InputIsHasSerbianLatinLetter_ReturnMostSimilar(string input, string expected)
+        {
+            string actual = _sut.GetMostSimilar(_stringList, input);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
--- LZRNS.Common/Comparers/TextComparer.cs
+++ LZRNS.Common/Comparers/TextComparer.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LZRNS.Common.Comparers
 {
@@ -6,7 +8,19 @@ namespace LZRNS.Common.Comparers
     {
         public string GetMostSimilar(IEnumerable<string> from, string to)
         {
-            return to;
+            int minDiff = int.MaxValue;
+            string mostSimilar = string.Empty;
+            foreach (string str in from)
+            {
+                int currDiff = LevenshteinDistance.Compute(str, to);
+                if (currDiff < minDiff)
+                {
+                    minDiff = currDiff;
+                    mostSimilar = str;
+                }
+            }
+
+            return mostSimilar;
         }
     }
 }
