9e333ddc6458a168f90a6cadc32715d9a7840e4f
milosradojcin <milos.radojcin.97@gmail.com>
2019-10-09 11:59:18 +0200

    Fixed team filtering

--- LZRNS.Web/js/pages/teams.js
+++ LZRNS.Web/js/pages/teams.js
@@ -1,38 +1,23 @@
 'use strict';
 
-let teams = {
-	init: function () {
-		$(document).on("click", "#all-teams", function () {
-			const $this = $(this);
+$(document).on("click", "#active-teams", function () {
+	const $this = $(this);
 
-			if ($this.hasClass("active")) {
-				$this.removeClass("active");
-				$("#all-teams").addClass("active");
-				$("#seasonYearStart").val("");
-				$("#teams-form").submit();
-			} else {
-				$this.addClass("active");
-				$("#all-teams").removeClass("active");
-				$("#seasonYearStart").val(this.dataset.seasonyearstart);
-				$("#teams-form").submit();
-			}
-		});
-
-		$(document).on("click", "#all-teams", function () {
-			const $this = $(this);
-			if ($this.hasClass("active")) {
-				$this.removeClass("active");
-				$("#active-teams").addClass("active");
-				$("#seasonYearStart").val(this.dataset.seasonyearstart);
-				$("#teams-form").submit();
-			} else {
-				$this.addClass("active");
-				$("#active-teams").removeClass("active");
-				$("#seasonYearStart").val("");
-				$("#teams-form").submit();
-			}
-		});
+	if (!$this.hasClass("active")) {
+		$this.addClass("active");
+		$("#all-teams").removeClass("active");
+		$("#seasonYearStart").val(this.dataset.seasonyearstart);
+		$("#teams-form").submit();
 	}
-};
+});
 
-module.exports = teams;
+$(document).on("click", "#all-teams", function () {
+	const $this = $(this);
+
+	if (!$this.hasClass("active")) {
+		$this.addClass("active");
+		$("#active-teams").removeClass("active");
+		$("#seasonYearStart").val("");
+		$("#teams-form").submit();
+	}
+});
